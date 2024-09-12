071914e5a326744e8fca268296a7258b7a95128f
Nixxa <52290162+sogeking-pixel@users.noreply.github.com>
2024-09-11 23:26:06 -0500

    Se implemento los botones para ordenar y desordenar el arreglo Animacion cuando el elemento se esta comparando

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const elements = document.getElementById("containerElements");
+const buttonOrdenar = document.getElementById("button_ordenar");
+const buttonDesordenar = document.getElementById("button_desordenar");
 function shuffle(array) {
   for (let i = array.length - 1; i >= 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -5,7 +8,6 @@ function shuffle(array) {
   }
   return array;
 }
-const elements = document.getElementById("containerElements");
 let progressBars = "";
 let cant = 27;
 let height = 100 / cant;
@@ -33,7 +35,8 @@ function ordenar() {
   let array = shuffledArray.slice(); // Copia del array para no modificar el original
   let i = 0;
   let j = 0;
-
+  buttonOrdenar.disabled = true;;
+  buttonDesordenar.disabled = true;;
   function step() {
     if (i < cant) {
       if (j < cant) {
@@ -49,7 +52,9 @@ function ordenar() {
       setTimeout(step, 20); // Ajusta el tiempo de espera según sea necesario
       }
     else {
-        ordenado(array);
+      ordenado(array);
+      
+      buttonDesordenar.disabled = false;
       }
       
   } 
@@ -88,4 +93,25 @@ function ordenado(array) {
         </div>`;
   }
   elements.innerHTML = progressBars;
+}
+
+
+function desordenar() {
+  if (buttonOrdenar.disabled) {
+    buttonOrdenar.disabled = false;
+  }
+  
+  shuffledArray = shuffle(arrayEelement);
+  let num;
+  progressBars = "";
+  for (let i = 0; i < shuffledArray.length; i++) {
+    num = shuffledArray[i];
+    progressBars += `<div class="progress mx-1" role="progressbar" aria-label="Basic example" aria-valuenow="${
+      height * num
+    }" aria-valuemin="0" aria-valuemax="100" >
+    <div class="progress-bar" style="height: ${height * num}%"></div>
+</div>`;
+  }
+  elements.innerHTML = progressBars;
+  
 }
