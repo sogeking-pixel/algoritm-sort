241db6dcaf93d46fc25924ac1f3f55a245b56243
Nixxa <52290162+sogeking-pixel@users.noreply.github.com>
2024-09-12 23:06:35 -0500

    Se implemento el algorimo insercion y seleccion Se separo en funciones los algoritmos Cambio la aparencia de las barras Se agrego la funcionalida de escoger el algoritmo a realizar

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const elements = document.getElementById("containerElements");
 const buttonOrdenar = document.getElementById("button_ordenar");
 const buttonDesordenar = document.getElementById("button_desordenar");
+const select = document.getElementById("algoritmoSelect");
+const time = 1;
+
 function shuffle(array) {
   for (let i = array.length - 1; i >= 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -9,7 +12,7 @@ function shuffle(array) {
   return array;
 }
 let progressBars = "";
-let cant = 50;
+let cant = 140;
 let height = 100 / cant;
 let arrayEelement = [];
 for (let i = 1; i <= cant; i++) {
@@ -19,49 +22,146 @@ for (let i = 1; i <= cant; i++) {
 let shuffledArray = shuffle(arrayEelement);
 let num;
 for (let i = 0; i < shuffledArray.length; i++) {
-    num = shuffledArray[i];
-  progressBars += `<div class="progress mx-1" role="progressbar" aria-label="Basic example" aria-valuenow="${
-    height * (num)
+  num = shuffledArray[i];
+  progressBars += `<div class="progress me-1" role="progressbar" aria-label="Basic example" aria-valuenow="${
+    height * num
   }" aria-valuemin="0" aria-valuemax="100" >
-    <div class="progress-bar" style="height: ${height * (num)}%"></div>
+    <div class="progress-bar" style="height: ${height * num}%"></div>
 </div>`;
 }
 
 elements.innerHTML = progressBars;
 
 
+//* falta implementar algoritmo de insercion
+function ordenar() {  
+  const algoritmo = select.value;
+  if (algoritmo === "") {
+    alert("Eliga un algoritmo:(");
+    return;
+  }
 
-function ordenar() {
   let array = shuffledArray.slice();
   buttonOrdenar.disabled = true;
   buttonDesordenar.disabled = true;
+  console.log(array);
+  switch (algoritmo) {
+    case '1':
+      burbuja(array);
+      break;
+    case '2':
+      seleccion(array);
+      break;
+    case '3':
+      insercion(array);
+      break;
+    default:
+      buttonOrdenar.disabled = false;
+      buttonDesordenar.disabled = false;
+      alert('todavia no se implementa :(');
+      break;
+  }
+}
 
-  let i = cant;
+function insercion(array) {
+  let i = 1;
+  let j = 0;
+  let aux = 0;
+  function bucle1() {
+    if (i < cant) {
+      aux = array[i];
+      j = i;
+      function bucle2() {
+        console.log("hola" + j);
+
+        if (array[j - 1] > aux && j > 0) {
+          array[j] = array[j - 1];
+          j--;
+          setTimeout(bucle2, time);
+          updateProgressBars(array, j - 1, j);
+        } else {
+          array[j] = aux;
+          updateProgressBars(array, j - 1, j);
+          i++;
+          setTimeout(bucle1, time);
+        }
+      }
+      bucle2();
+    } else {
+      console.log(array);
+      ordenado(array);
+      buttonDesordenar.disabled = false;
+    }
+  }
+  bucle1();
+  
+}
+
+function seleccion(array) {
+  let i = 0;
+  let k = 0;
+  let j = 0;
+  let aux = 0;
+
+  function bucle1() {
+    if (i < cant) {
+      aux = array[i];
+      k = i;
+      j = i;
+      function bucle2() {
+        if (j < cant - 1) {
+          j++;
+          if (array[j] < aux) {
+            aux = array[j];
+            k = j;
+          }
+          setTimeout(bucle2, time);
+          updateProgressBars(array, k, j);
+        } else {
+          array[k] = array[i];
+          array[i] = aux;
+          updateProgressBars(array, i, k);
+          i++;
+          setTimeout(bucle1, time);
+        }
+      }
+      bucle2();
+    } else {
+      console.log(array);
+
+      ordenado(array);
+      buttonDesordenar.disabled = false;
+    }
+  }
+
+  bucle1();
+}
+
+function burbuja(array) {
+  let i = 0;
   let j = 0;
 
-  function ordenarPaso() {
-    if (i > 0) {
-      if (j < i - 1) {
+  function bucle1() {
+    if (i < cant) {
+      if (j < cant) {
         if (array[j] > array[j + 1]) {
-          [array[j], array[j + 1]] = [array[j + 1], array[j]];         
+          [array[j], array[j + 1]] = [array[j + 1], array[j]];
         }
-         updateProgressBars(array, j, j + 1);
+        updateProgressBars(array, j, j + 1);
         j++;
-        setTimeout(ordenarPaso, 10); // Retraso de 300 ms entre cada paso
+        setTimeout(bucle1, time);
       } else {
         j = 0;
-        i--;
-        setTimeout(ordenarPaso, 0); // Retraso de 300 ms entre cada paso
+        i++;
+        setTimeout(bucle1, 0);
       }
     } else {
       ordenado(array);
       buttonDesordenar.disabled = false;
     }
   }
-
-  ordenarPaso();
+  bucle1();
 }
-
 
 
 function updateProgressBars(array, i, j) {
@@ -72,9 +172,9 @@ function updateProgressBars(array, i, j) {
     if (k === i) {
       className = "highlight-current";
     } else if (k === j) {
-        className = "highlight-compare";
+      className = "highlight-compare";
     }
-    progressBars += `<div class="progress mx-1 ${className}" role="progressbar" aria-label="Basic example" aria-valuenow="${
+    progressBars += `<div class="progress me-1 ${className}" role="progressbar" aria-label="Basic example" aria-valuenow="${
       height * num
     }" aria-valuemin="0" aria-valuemax="100" >
             <div class="progress-bar" style="height: ${height * num}%"></div>
@@ -87,8 +187,8 @@ function ordenado(array) {
   let progressBars = "";
   for (let k = 0; k < array.length; k++) {
     let num = array[k];
-    
-    progressBars += `<div class="progress mx-1 highlight-comp" role="progressbar" aria-label="Basic example" aria-valuenow="${
+
+    progressBars += `<div class="progress me-1 highlight-comp" role="progressbar" aria-label="Basic example" aria-valuenow="${
       height * num
     }" aria-valuemin="0" aria-valuemax="100" >
             <div class="progress-bar" style="height: ${height * num}%"></div>
@@ -97,23 +197,21 @@ function ordenado(array) {
   elements.innerHTML = progressBars;
 }
 
-
 function desordenar() {
   if (buttonOrdenar.disabled) {
     buttonOrdenar.disabled = false;
   }
-  
+
   shuffledArray = shuffle(arrayEelement);
   let num;
   progressBars = "";
   for (let i = 0; i < shuffledArray.length; i++) {
     num = shuffledArray[i];
-    progressBars += `<div class="progress mx-1" role="progressbar" aria-label="Basic example" aria-valuenow="${
+    progressBars += `<div class="progress me-1" role="progressbar" aria-label="Basic example" aria-valuenow="${
       height * num
     }" aria-valuemin="0" aria-valuemax="100" >
     <div class="progress-bar" style="height: ${height * num}%"></div>
 </div>`;
   }
   elements.innerHTML = progressBars;
-  
 }
